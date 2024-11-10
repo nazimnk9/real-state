135d04a5fefd09be700584d16901ad6e929390eb
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-11-10 18:18:08 +0600

    forgot password set

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,7 @@ import * as config from "../config.js";
 import jwt from "jsonwebtoken";
 import { emailTemplate } from "../helpers/email.js";
 import { hashPassword, comparePassword } from "../helpers/auth.js";
-import User from "../models/auth.js";
+import User from "../models/user.js";
 import { nanoid } from "nanoid";
 //import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
 import Validator from "email-validator";
@@ -40,19 +40,19 @@ export const preRegister = async (req, res) => {
     //console.log(req.body);
      const { email, password } = req.body;
 
-    // if (!Validator.validate(email)) {
-    //   return res.json({ error: "A valid email is required" });
-    // }
-    // if (!password) {
-    //   return res.json({ error: "Password is required" });
-    // }
-    // if (password && password.length < 8) {
-    //   return res.json({ error: "Password should be at least 8 characters" });
-    // }
-    // const userEmail = await User.findOne({ email });
-    // if (userEmail) {
-    //   return res.json({ error: "Email is taken" });
-    // }
+    if (!Validator.validate(email)) {
+      return res.json({ error: "A valid email is required" });
+    }
+    if (!password) {
+      return res.json({ error: "Password is required" });
+    }
+    if (password && password?.length < 8) {
+      return res.json({ error: "Password should be at least 8 characters" });
+    }
+    const userEmail = await User.findOne({ email });
+    if (userEmail) {
+      return res.json({ error: "Email is taken" });
+    }
     // //const result = await createUserWithEmailAndPassword(auth, email, password);
     
     const token = jwt.sign({ email, password }, config.JWT_SECRET, {
@@ -85,7 +85,7 @@ export const preRegister = async (req, res) => {
 
     transporter.sendMail(emailTemplate(email, `
       <p>Please click the link below to activate your account.</p>
-          <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>
+      <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>
       `,config.REPLY_TO,"Activate Your Account"), (error, info) => {
       if (error) {
         console.log(error);
@@ -113,12 +113,25 @@ export const register = async (req, res) => {
 //       return res.json({ error: "Email is taken" });
 //     }
 //     //const { email, password } = jwt.verify(req.body.token, config.JWT_SECRET);
-//     const hashedPassword = await hashPassword(password);
-//     const user = await new User({
-//       username: nanoid(6),
-//       email,
-//       password: hashedPassword,
-//     }).save();
+       const hashedPassword = await hashPassword(password);
+       const user = await new User({
+          username: nanoid(6),
+          email,
+          password: hashedPassword,
+       }).save();
+       const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+       const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      user.password = undefined
+      user.resetCode = undefined
+      return res.json({
+        token,
+        refreshToken,
+        user,
+      })
 //     tokenAndUserResponse(req, res, user);
    } catch (err) {
      console.log(err);
@@ -126,23 +139,36 @@ export const register = async (req, res) => {
    }
 };
 
-// export const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
+ export const login = async (req, res) => {
+   try {
+     const { email, password } = req.body;
 //     // 1 find user by email
-//     const user = await User.findOne({ email });
+     const user = await User.findOne({ email });
 //     // 2 compare password
-//     const match = await comparePassword(password, user.password);
-//     if (!match) {
-//       return res.json({ error: "Wrong password" });
-//     }
+     const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.json({ error: "Wrong password" });
+    }
+    const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+     const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    user.password = undefined
+      user.resetCode = undefined
+      return res.json({
+        token,
+        refreshToken,
+        user,
+      })
 
 //     tokenAndUserResponse(req, res, user);
-//   } catch (err) {
-//     console.log(err);
-//     return res.json({ error: "Something went wrong. Try again." });
-//   }
-// };
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "Something went wrong. Try again." });
+  }
+ };
 
 // /* export const forgotPassword = async (req, res) => {
 //   try {
@@ -177,31 +203,43 @@ export const register = async (req, res) => {
 //   }
 // }; */
 
-// export const forgotPassword = async (req, res) => {
-//   try {
-//     const { email } = req.body;
+export const forgotPassword = async (req, res) => {
+   try {
+     const { email } = req.body;
 
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.json({ error: "Could not find user with that email" });
-//     } else {
-//       const resetCode = nanoid();
-//       user.resetCode = resetCode;
-//       await user.save();
+     const user = await User.findOne({ email });
+     if (!user) {
+       return res.json({ error: "Could not find user with that email" });
+     } else {
+       const resetCode = nanoid();
+       user.resetCode = resetCode;
+       await user.save();
 
-//       const token = jwt.sign({ resetCode }, config.JWT_SECRET, {
-//         expiresIn: "1h",
-//       });
+      const token = jwt.sign({ resetCode }, config.JWT_SECRET, {
+        expiresIn: "1h",
+      });
 
-//       const transporter = nodemailer.createTransport({
-//         /* Configure your email transport settings here */
-//         // For example, if you're using Gmail as your SMTP server:
-//         service: 'Gmail',
-//         auth: {
-//           user: 'mdnazimahmed64@gmail.com',
-//           pass: 'lkzdawncfqmngjdb',
-//         },
-//       });
+      const transporter = nodemailer.createTransport({
+        /* Configure your email transport settings here */
+        // For example, if you're using Gmail as your SMTP server:
+        service: 'Gmail',
+        auth: {
+          user: 'mdnazimahmed64@gmail.com',
+          pass: 'wrdrygfjnqgygixe',
+        },
+      });
+      transporter.sendMail(emailTemplate(email, `
+        <p>Please click the link below to access your account.</p>
+        <a href="${config.CLIENT_URL}/auth/access-account/${token}">Access my account</a>
+        `,config.REPLY_TO,"Access Your Account"), (error, info) => {
+        if (error) {
+          console.log(error);
+          return res.json({ ok: false });
+        } else {
+          console.log(info);
+          return res.json({ ok: true });
+        }
+      });
 
 //       const mailOptions = {
 //         from: 'mdnazimahmed64@gmail.com', // Sender's email address
@@ -222,12 +260,12 @@ export const register = async (req, res) => {
 //           return res.json({ token });
 //         }
 //       });
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     return res.json({ error: "Something went wrong. Try again." });
-//   }
-// };
+     }
+  } catch (err) {
+    console.log(err);
+    return res.json({ error: "Something went wrong. Try again." });
+  }
+ };
 
 
 // export const accessAccount = async (req, res) => {
