c28b575201f45da75a2901034638082a158f5289
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-12-17 16:48:38 +0600

    solve and button

---
+++ client/src/components/forms/AdForm.js
@@ -0,0 +1,8 @@
+export default function AdForm({action,type}) { 
+    return (
+        <>
+        <p>This is ad create form</p>
+        {action}/{type}
+        </>
+    )
+ }
--- client/src/pages/user/Ad/AdCreate.js
+++ client/src/pages/user/Ad/AdCreate.js
@@ -22,7 +22,7 @@ export default function AdCreate() {
         <div>
             <h1 className="display-1 bg-primary text-light p-5">Ad Create</h1>
             <Sidebar />
-            <div className="d-flex justify-content-center align-items-center vh-100" style={{marginTop:"-10%"}}>
+            <div className="d-flex justify-content-center align-items-center vh-100" style={{marginTop:"-16%"}}>
                 <div className="col-lg-6">
                     <button onClick={handleSell} className="btn btn-primary btn-lg col-12 p-5"><span className="h2">Sell</span></button>
                     {sell && (<div className="my-1">
