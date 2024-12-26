872b841479d661e10b996e4e7d61bbf67fa524c4
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2024-12-26 10:03:42 +0600

    solve image resize problem

--- client/src/components/forms/ImageUpload.js
+++ client/src/components/forms/ImageUpload.js
@@ -1,12 +1,48 @@
+import Resizer from "react-image-file-resizer";
+import axios from "axios"
+
 export default function ImageUpload({ ad, setAd }) {
   const handleUpload = async (e) => {
     try {
         let files = e.target.files
         files = [...files]
         if(files?.length){
-            console.log(files);
+            //console.log(files);
             
             setAd({ ...ad, uploading: true });
+            files.map((file) =>{
+              new Promise(()=>{
+                Resizer.imageFileResizer(
+                  file,
+                  1080,
+                  720,
+                  "JPEG",
+                  100,
+                  0,
+                  async (uri) => {
+                    try{
+                      console.log("upload url=>",uri);
+                      
+                      const {data} = await axios.post("/upload-image",{
+                        image: uri,
+                      });
+                      setAd((prev)=>(
+                        {
+                          ...prev,
+                          photos: [data,...prev.photos],
+                          uploading: false
+                        }
+                      ))
+                    }catch(err){
+                      console.log(err);
+                      setAd({...ad, uploading: false})
+                      
+                    }
+                  },
+                  "base64"
+                );
+              })
+            })
         }
     } catch (err) {
       console.log(err);
