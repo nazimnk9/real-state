ec1c1cbd9926d3eb66f71d601619b41ee8dcd2a6
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-03-23 12:32:15 +0600

    disabled contact seller form without logged in

--- client/src/components/forms/ContactSeller.js
+++ client/src/components/forms/ContactSeller.js
@@ -13,6 +13,7 @@ export default function ContactSeller({ ad }) {
     const [loading, setLoading] = useState("")
     //hooks
     const navigate = useNavigate();
+    const loggedIn = auth.user !== null && auth.token !== "";
     return <>
         <div className="row">
             <div className="col-lg-8 offset-lg-2">
@@ -20,6 +21,52 @@ export default function ContactSeller({ ad }) {
                     Contact{" "}
                     {ad?.postedBy?.name ? ad?.postedBy?.name : ad?.postedBy?.username}
                 </h3>
+                <form>
+                    <textarea
+                        name="message"
+                        className="form-control mb-3"
+                        placeholder="" wrap="Write your message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        autoFocus={true}
+                        disabled={!loggedIn}
+                    ></textarea>
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        disabled={!loggedIn}
+                    />
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        disabled={!loggedIn}
+                    />
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter your phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        disabled={!loggedIn}
+                    />
+                    <button
+                        className="btn btn-primary mt-4 mb-5"
+                        disabled={!name || !email || loading}
+                    >
+                        {loggedIn
+                            ?
+                            loading
+                                ? "Please Wait"
+                                : "Send Enquiry"
+                            : "Login to send enquiry"}
+                    </button>
+                </form>
             </div>
         </div>
     </>;
